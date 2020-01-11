8e4f4951bb75dfc673d791a5de3eedbcdd8dcdb2
Ferry <ferryhermawan864@gmail.com>
2020-01-11 14:17:29 +0700

    Tugas 24 JS

---
+++ tugas24.js
@@ -0,0 +1,9 @@
+function panggilsort(){
+var angka = [32, 33, 16, 40, 9, 7, 11, 2, 44, 1, 66];
+console.log("Sebelumnya :",angka.toString());
+angka = angka.sort();
+console.log("Ascending :",angka.toString());
+revangka = angka.reverse();
+console.log("Descending :",revangka.toString());
+}
+panggilsort();
